7b0361e31ffc21c512e74d7854afe7344cb3b5ec
tash2cool <desi.dreamer@hotmail.com>
2025-01-06 01:18:47 -0500

    updates current time and date

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ function updateWeather(response) {
   windHeader.innerHTML = wind;
   let humidityHeader = document.querySelector("#humidity");
   humidityHeader.innerHTML = response.data.temperature.humidity;
+  let details = document.querySelector("#weather-details");
+  details.innerHTML = response.data.condition.description;
 }
 //updates api url to the corresponding city that was searched
 function updateUrl(city) {
@@ -23,12 +25,62 @@ function updateCity(event) {
   city = city.value.trim();
   updateUrl(city);
 }
+function setDate() {
+  let date = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let number = date.getDate();
+  let hour = date.getHours();
+  let setting;
+  if (hour < 13) {
+    setting = "am";
+  } else {
+    hour = hour - 12;
+    setting = "pm";
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let year = date.getFullYear();
+  let fullDate = `${day}, ${month} ${number}, ${year}`;
+  let dateHeader = document.querySelector("#current-date");
+  dateHeader.innerHTML = fullDate;
+  let fullTime = `${hour}:${minutes} ${setting}`;
+  let timeHeader = document.querySelector("#current-time");
+  timeHeader.innerHTML = fullTime;
+  console.log(date);
+}
 //variables to update city header
 let cityForm = document.querySelector("#search-city");
-console.log(cityForm);
 cityForm.addEventListener("submit", updateCity);
-//variables for API
+//variables for API, sets the current data to Montreal
 let apiKey = "7ae0d3164bde28fe1dababf583380ot9";
 let city = "Montreal";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiUrl).then(updateWeather);
+//update the date
+setDate();
