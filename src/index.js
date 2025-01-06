762248aa2c90d0fdf96943c2aa769ef451f18945
tash2cool <desi.dreamer@hotmail.com>
2025-01-06 00:26:59 -0500

    updates city header
    -changes the city header to whatever is searched in the search box form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-//alert("hi");
+function updateCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  let cityHeader = document.querySelector("#city-header");
+  cityHeader.innerHTML = city.value;
+}
+let cityForm = document.querySelector("#search-city");
+console.log(cityForm);
+cityForm.addEventListener("submit", updateCity);
